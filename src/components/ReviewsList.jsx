024aa5ba1036873c72ad2032403c77917a991f50
Roseanna Craggs <roseannacraggs21@gmail.com>
2023-02-09 15:37:24 +0000

    task 10, not ideal css on buttons/showing user the selected order and sort options

--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -8,16 +8,23 @@ const ReviewsList = () => {
 
   const [reviewsList, setReviewsList] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
+  const [sortBy, setSortBy] = useState("created_at");
+  const [orderBy, setOrderBy] = useState("desc");
+
+  let url = "/reviews"
+
+  if (sortBy && orderBy) {
+    url = url.concat(`?sort_by=${sortBy}&order=${orderBy}`)
+  }
 
   useEffect(() => {
     setIsLoading(true)
-
-    gamesAPI.get("/reviews")
+    gamesAPI.get(url)
     .then(({data}) => {
       setReviewsList(data)
       setIsLoading(false)
     });
-  }, [])
+  }, [sortBy, orderBy])
 
   if (isLoading) {
     return <p>Loading reviews...</p>
@@ -27,7 +34,14 @@ const ReviewsList = () => {
     <>
     <h2 id='reviews-title'>Reviews</h2>
     <h3>Browse through the reviews written by fellow board game lovers.</h3>
-    <h4>Click on review titles to read more!</h4>
+    <h4>Click on a review to read more!</h4>
+    <p>Sort by: {sortBy}, {orderBy}</p>
+    <button class="clicked" onClick={() => setSortBy("created_at")}>Date/Time (default)</button>
+    <button class="clicked" onClick={() => setSortBy("comment_count")}>Number of Comments</button>
+    <button class="clicked" onClick={() => setSortBy("votes")}>Number of Votes</button>
+    <p>Order:</p>
+    <button class="clicked" onClick={() => setOrderBy("desc")}>Descending (default)</button>
+    <button class="clicked" onClick={() => setOrderBy("asc")}>Ascending</button>
     <ul>
       {reviewsList.map((review) => {
         return <SingleReview key={review.review_id} review={review}/>
